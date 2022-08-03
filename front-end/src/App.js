c33020ef7dd3402f0fd22ab75f7171ce3c6e58aa
Shirzad Zeynal <z_shirzad@myself.com>
2022-08-03 13:59:31 +0430

    Video-03-Create-React-App

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+// feature 1
+import data from './data.json'
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+    </div>
+  )
+}
+
+export default App
