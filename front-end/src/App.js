9c04a4d2c5c9d64b3b13974a4bb98ce8fe9e2773
Shirzad Zeynal <z_shirzad@myself.com>
2022-08-03 20:47:13 +0430

    Add bootstrap UI Framework

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,34 @@
-// feature 1
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import ProductPage from './pages/product/ProductPage'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="grid-container">
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">React Shopping Cart</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>Shopazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/product/:slug' element={<ProductPage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/product/:slug' element={<ProductPage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   )
