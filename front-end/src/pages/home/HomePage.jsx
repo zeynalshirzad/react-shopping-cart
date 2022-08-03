44a7aceaaada96b75bb1248fe5aa397353c1cd60
Shirzad Zeynal <z_shirzad@myself.com>
2022-08-03 18:04:55 +0430

    Fetch Products From Backend

--- front-end/src/pages/home/HomePage.jsx
+++ front-end/src/pages/home/HomePage.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react'
 import Products from '../../components/Products'
-import data from '../../data.js'
+// import data from '../../data.js'
+import axios from 'axios'
 
 export default function HomePage() {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+
+            const response = await axios.get('/api/products')
+            setProducts(response.data)
+            setLoading(false)
+        }
+        fetchProducts()
+    }, [])
     return (
-        <Products products={data.products} />
+        <>
+            {loading ? <h1>Data is loading ...</h1> : <Products products={products} />}
+        </>
+
     )
 }
