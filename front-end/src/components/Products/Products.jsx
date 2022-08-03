8c22b330bb285ca73215239e3a7e277c7c2327b6
Shirzad Zeynal <z_shirzad@myself.com>
2022-08-03 21:42:49 +0430

    move fetch data to products component

--- front-end/src/components/Products/Products.jsx
+++ front-end/src/components/Products/Products.jsx
@@ -1,15 +1,43 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useAuthDispatch, useAuthState } from "../../context"
+import { fetchFailed, fetchProducts, fetchSucceed } from "../../context/reducer"
 import Product from "../Product"
 import './products.css'
 
-export default function Products({ products }) {
-    const productCms = products.map(product => (
+export default function Products() {
+
+    const dispatch = useAuthDispatch()
+    const { loading, error, products } = useAuthState()
+
+    useEffect(() => {
+        const loadProducts = async () => {
+            dispatch(fetchProducts())
+            try {
+                const response = await axios.get('/api/products')
+                dispatch(fetchSucceed(response.data))
+            } catch (err) {
+                dispatch(fetchFailed('There is an error in fetching data'))
+            }
+
+        }
+        loadProducts()
+    }, [dispatch])
+
+    const productCms = products?.map(product => (
         <Product key={product.id} product={product} />
     ))
+
+    const loadCm = <h1>Data is loading ...</h1>
+    const errorCm = <h1>{error}</h1>
+
     return (
         <>
             <h1>Featured Products</h1>
             <div className="products">
-                {productCms}
+                {loading ? loadCm :
+                    error ? errorCm :
+                    productCms}
             </div>
         </>
     )
--- front-end/src/pages/home/HomePage.jsx
+++ front-end/src/pages/home/HomePage.jsx
@@ -1,37 +1,8 @@
-import { useEffect } from 'react'
 import Products from '../../components/Products'
-import axios from 'axios'
-import { useAuthDispatch, useAuthState } from '../../context'
-import { fetchFailed, fetchProducts, fetchSucceed } from '../../context/reducer'
 
 export default function HomePage() {
 
-    const dispatch = useAuthDispatch()
-    const { loading, error, products } = useAuthState()
-
-    useEffect(() => {
-        const loadProducts = async () => {
-            dispatch(fetchProducts())
-            try {
-                const response = await axios.get('/api/products')
-                dispatch(fetchSucceed(response.data))
-            } catch (err) {
-                dispatch(fetchFailed('There is an error in fetching data'))
-            }
-
-        }
-        loadProducts()
-    }, [dispatch])
-
-    const loadCm = <h1>Data is loading ...</h1>
-    const errorCm = <h1>{error}</h1>
-
     return (
-        <>
-            {loading ? loadCm :
-                error ? errorCm :
-                    <Products products={products} />}
-        </>
-
+        <Products />
     )
 }
