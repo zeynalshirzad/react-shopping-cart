f15b56d6c389ee018fc4df4da9ee40849aeaa9bc
Shirzad Zeynal <z_shirzad@myself.com>
2022-08-15 19:42:40 +0430

    Heroku work with PORT

--- back-end/server.js
+++ back-end/server.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
 })
 
-const port = process.env.SERVER_PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
 })
